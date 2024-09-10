a260f53395554876ce3e135d68e03d62d71f636f
Ashutosh more <ashutosh.more@expleogroup.com>
2024-09-10 16:53:06 +0530

    assignment file added

---
+++ Assignment1.js
@@ -0,0 +1,84 @@
+// 1 Write a single line comment which says, comments can make code readable
+// ------comments can make code readable
+
+// 2 Write another single comment which says, Welcome to 30DaysOfJavaScript
+// ------Welcome to 30DaysOfJavaScript
+
+// 3 Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*comments can make code readable, 
+easy to reuse and informative*/
+
+// 4 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// let A = "Ashutosh"// String
+// let B = True  //Boolean
+// let C = undefined  // undefined
+// let D = null  //null
+
+// 5 Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// console.log(typeof(A));
+// console.log(typeof(B));
+// console.log(typeof(C));
+// console.log(typeof(D));
+// console.log("A");
+// 6 Declare four variables without assigning values
+// E
+// F
+// G
+// H
+
+// 7 Declare four variables with assigned values
+// Name = "Ashtosh"
+// Sirname = "More"
+// salary = 89898
+// value = True
+
+// 8 Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// Name = "Ashutosh"
+// Sirname = "More"
+// Maritual_status = "Married"
+// Age = 26
+
+// 9 Declare variables to store your first name, last name, marital status, country and age in a single line
+// Name = "Ashutosh",Sirname = "More",Maritual_status = "Married", Age = 26
+
+// 10 Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+// My_age = "I am 25 years old"
+// Your_age = "You are 30 year old"
+
+// I am 25 years old.
+// You are 30 years old.
+
+// Assignmrnt2 
+
+// 1 Generate a random number between 0 and 100 inclusively.
+let num = (Math.random()*100)
+console.log(num);
+let n3 = Math.floor(Math.random()*100)
+console.log(n3);
+let n4 = Math.floor(Math.random()*100)
+console.log(n4);
+let n5 = Math.ceil(Math.random()*100)
+console.log(n5);
+
+let n1 = Math.ceil(Math.random()*100)
+console.log(n1);
+// 2 Generate a random number between 50 and 100 inclusively.
+let val = Math.floor(Math.random()*51) + 50
+console.log(val);
+// 3 Generate a random number between 0 and 255 inclusively 
+let val1 = Math.floor(Math.random()*256)
+console.log(val1);
+// 4 create a program to get the output if a dice is roll out
+function diece1(){
+console.log("you rolled a dice "   +  Math.floor(Math.random()* 6));
+}
+
+diece1()
+// 5 create a program to get the output if 2 dice are  rolled out
+function diece2(){
+    console.log("you rolled a dice "   +  Math.floor(Math.random()* 6));
+    console.log("you rolled a dice "   +  Math.floor(Math.random()* 6));
+    }
+    
+    diece2()
+    
